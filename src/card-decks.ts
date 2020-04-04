8170aa88c3383c576b63285b09e6b99d519f5331
Michael Kimball <m.d.a.kimball@gmail.com>
2020-04-04 12:34:09 -0400

    Add webpack build config

---
+++ src/card-decks.ts
@@ -0,0 +1,1 @@
+let hoodoo:Number = 1;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+const CopyPlugin = require('copy-webpack-plugin');
+
+let config = JSON.parse(fs.readFileSync('./foundryconfig.json'));
+let name = JSON.parse(fs.readFileSync('./module.json')).name;
+let linkDir;
+let destDir = 'modules';
+if (config.dataPath) {
+    if (!fs.existsSync(path.join(config.dataPath, 'Data')))
+        throw Error('User Data path invalid, no Data directory found');
+    linkDir = path.join(config.dataPath, 'Data', destDir, name);
+} else {
+    throw Error('No User Data path defined in foundryconfig.json');
+}
+
+module.exports = {
+  entry: ['./src/card-decks.ts'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'module.json', to: '' }
+    ]),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, linkDir),
+  },
+};
